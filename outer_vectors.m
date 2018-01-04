e1e007a1bbb15de03723fd2fea1a84f003b66d28
Caio Garcez <caio.garcez1@gmail.com>
2018-01-04 20:20:12 -0200

    Update outer_vectors.m

--- outer_vectors.m
+++ outer_vectors.m
@@ -1,23 +1,31 @@
 function [ Tensor_Xo ] = outer_vectors( V1, V2, V3)
 %PAFAFAC_OUTER Summary of this function goes here
-%   Detailed explanation goes here
+
+%%  Extraction of the variables. 
 
 [number_of_samples, d] = size(V3); % Dimensions of the third matrix. 
+size_V1= size(V1); 
+size_V2= size(V2); 
+size_V3= size(V3); 
 
-Tensor_Xo = zeros(5, 6, 8); % Inicialization of the output tensor. 
+Tensor_Xo = zeros(size_V1(1,1), size_V2(1,1), size_V3(1,1)); % Output tensor. 
+
+Face = zeros(size_V1(1,1), size_V2(1,1), size_V3(1,1)); % Intermadiate varible. 
+
+%% Initialization of the algorithm. 
 
 for i = 1:d 
-    % Matrix of the outer product between the vectors of the first two matrices.
-    AUX = V1(:,i) * V2(:,i).'; 
     
-    Tensor_Xo_aux = zeros(5, 6, 8);  % Inicialization of the auxiliar tensor.
+    Aux = V1(:,i) * V2(:,i).';  % Matrix of the outer product between the vectors of the first two matrices.
     
-for k = 1:number_of_samples % ammount of lines of the matrix V3. 
+        for k = 1:number_of_samples
+        
+            Face(:,:,k) = Aux * V3(k,i);
     
-    Tensor_Xo_aux(:,:,k) = AUX * V3(k,d); % Obtain a rank1 tensor. 
+        end
     
-end
-
-Tensor_Xo = Tensor_Xo + Tensor_Xo_aux; % Sum of the d rank1 vectors. 
-
+    Tensor_Xo = Tensor_Xo + Face; 
+        
+    Face(:,:,:) = zeros(size_V1(1,1), size_V2(1,1), size_V3(1,1)); 
+    
 end
