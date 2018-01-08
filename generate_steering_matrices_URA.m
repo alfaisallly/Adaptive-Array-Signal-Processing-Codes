ece2ab7df8097beb5b9f4109998472f69ca342c4
Caio Garcez <caio.garcez1@gmail.com>
2018-01-08 17:05:48 -0200

    Update generate_steering_matrices_URA.m

--- generate_steering_matrices_URA.m
+++ generate_steering_matrices_URA.m
@@ -1,25 +1,47 @@
 function [ steering_matrix_A , steering_matrix_B, azimuths, elevations ] = generate_steering_matrices_URA( size_A , size_B )
-% --------------------------------------------------------------------------------------
-% Generate a steering matrix for a uniform rectangular array with size: rows x collumns. 
-% The scenario considers Delta = Lambda/2. 
 
-%% Extraction of the parameters. 
+%{
+
+Funtion: Generate steering matrices URA.
+
+Generate two steering matrices for a Uniform Rectangular Array - URA. 
+
+Inputs: size_A -- [a b] -- row vector containing the dimensions of the desired steering matrix A. 
+        size_B -- [a b] -- row vector containing the dimensions of the desired steering matrix B. 
+
+Outputs: steering_matrix_A -- Matrix of size: a x b.
+         steering_matrix_B -- Matrix of size: a x b.
+         azimuths -- d angles of azimuth.
+         elevations -- d angles of elevation.
+
+Observations:
+
+d is considered the model order.
+Delta = lambda/2. Space between each sensors is equal to half of the wavelength of the signal.
+
+%} 
+
+%% Extract the desired dimensions.
+
 M1 = size_A(1,1);
 M2 = size_B(1,1);
-d = size_A(1,2); 
+d = size_A(1,2); % Must be also equal to size_B(1,2); 
 
 %% Generation of random angles of arrival for azimuth and elevation. 
+
 k1 = rand(d,1); 
-azimuths = (k1.*pi) - (pi/2); 
+azimuths = (k1.*pi) - (pi/2); % Limited from: -pi/2 to pi/2;
 
 k2 = rand(d,1);
 elevations = (k2.*pi) - (pi/2);
 
-%% Generation of the spatial frequencies. 
+%% Generation of the spatial frequencies. Considering: Delta = lambda/2. 
+
 spatial_freqs_r1 = pi * cos(azimuths) .* sin(elevations);
 spatial_freqs_r2 = pi * sin(azimuths) .* sin(elevations);
 
 %% Generation of the steering matrices.
+
 steering_matrix_A = (0:1:M1-1)'*(spatial_freqs_r1');
 steering_matrix_A = exp(1i * steering_matrix_A );
 
